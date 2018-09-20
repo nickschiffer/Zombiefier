19a191a3137cfbe04c2cc2346f489d63b5b0791b
nickschiffer <nick@schiffer.us>
2018-09-20 13:34:31 -0700

    Added SIGCONT signal handler

--- zombiefier.c
+++ zombiefier.c
@@ -8,10 +8,18 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define PSSCRIPT "ps aux | awk \'\"[Zz]\" ~ $8 { printf(\"%s, PID = %d\\n\", $8, $2); }\'"
+
+void sigcont_handler(int signum){
+    printf("SIGCONT recieved for PID #%d\n",signum);
+    exit(signum);
+}
+
 int main(int argc, char *argv[]) 
-{ 
+{    
+     int parent_PID = 0;
      if (argc != 3){
          printf("Incorrect Format.\nUsage: zombiefier -n [numZombies].\n");
          return EXIT_FAILURE;
@@ -25,12 +33,13 @@ int main(int argc, char *argv[])
          printf("Only a positive number of zombies is allowed.\n");
          return EXIT_FAILURE;
      }
-     printf("%d Zombies Requested.\n\n", numZombies);
+     printf("\n%d Zombies Requested.\n\n", numZombies);
     
     for (int i = 0; i < numZombies; i++){
         pid_t pid = fork();
         if (pid > 0){
-            printf("parent PID: %d\n", getpid());
+            parent_PID = getpid();
+            printf("parent PID: %d\n", parent_PID);
             sleep(1);
         }
         else if (pid == 0){
@@ -42,10 +51,17 @@ int main(int argc, char *argv[])
             return EXIT_FAILURE;
         }
     }
-    printf("Press t to view ps zombies output or e to exit.\n\n");
-    char c = getchar();
-    if (c == 't'){system(PSSCRIPT);}
-    printf("\nZombies killed, exiting.\n");
+    printf("Zombie Processes from parent PID #%d via ps:\n\n", parent_PID);
+    system(PSSCRIPT);
+    signal(SIGCONT, sigcont_handler);
+    printf("\nSend \"kill -SIGCONT %d\" in separate console window to kill Zombies.\n", parent_PID);
+    while (1){
+        sleep(1);
+    }
+    // printf("Press t to view ps zombies output or e to exit.\n\n");
+    // char c = getchar();
+    // if (c == 't'){system(PSSCRIPT);}
+    // printf("\nZombies killed, exiting.\n");
 
 	return EXIT_SUCCESS; 
 }
